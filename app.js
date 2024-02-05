02838c0089ca89e084709d09c6eaeb05fef3cd61
Kyle George <123008256+WileKoylote@users.noreply.github.com>
2024-02-04 22:46:47 -0600

    added functions

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-let name = prompt("what is your name?");
-    console.log(`story ${name} story`);
+    let name = prompt("what is your name?");
+    console.log('story {name} story');
 
     //Global
     var me = "human";
@@ -16,15 +16,19 @@ let name = prompt("what is your name?");
     }
 
     //Object
-    const cupOfjoe = {m:"M", o:"O", c:"C", h:"H", a:"A"};
-          cupOfjoe.makeCoffee = function() {
-            console.log ('{this.m + this.o + this.c + this.h + this.a}');
-          };
-          cupOfjoe.orderUp();
+    const cupOfjoe = {m:"M", o:"O", c:"C", h:"H", a:"A",
+          orderUp: function() {console.log(this.m + this.o + this.c + this.h + this.a)}};
           
+          cupOfjoe.orderUp();
 
-    //Functions
-  
+    //Function
+    const coffeeTopping = "Add Cream";
 
+    function makeCream(ing1, ing2){
+    console.log(coffeeTopping);
+    console.log("Mix" + ing1 + "with" + ing2)};
+
+    makeCream("milk", "cream");
     
 
+    
