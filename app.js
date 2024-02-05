cc3f197cc193961397ee8cef214eebc46ad7ce46
Kyle George <123008256+WileKoylote@users.noreply.github.com>
2024-02-04 19:02:25 -0600

    Button Fix

---
+++ app.js
@@ -0,0 +1,30 @@
+let name = prompt("what is your name?");
+    console.log(`story ${name} story`);
+
+    //Global
+    var me = "human";
+
+    /*
+    var myString = "I'm" + "" + "Alive";
+    */
+
+    //Math Block
+    {
+        let x = 100;
+        let y = 1;
+        let z = x - y;
+    }
+
+    //Object
+    const cupOfjoe = {m:"M", o:"O", c:"C", h:"H", a:"A"};
+          cupOfjoe.makeCoffee = function() {
+            console.log ('{this.m + this.o + this.c + this.h + this.a}');
+          };
+          cupOfjoe.orderUp();
+          
+
+    //Functions
+  
+
+    
+
